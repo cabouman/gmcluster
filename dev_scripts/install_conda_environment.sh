022beeecdfced43c083700369e548892336b9038
Charles Bouman <Charles.Bouman@gmail.com>
2022-05-17 09:35:02 -0400

    Update install scripts to activate conda environment

--- dev_scripts/install_conda_environment.sh
+++ dev_scripts/install_conda_environment.sh
@@ -3,7 +3,10 @@
 
 # Create and activate new conda environment
 cd ..
+conda deactivate
+conda deactivate
 conda remove env --name pygmcluster --all
 conda create --name pygmcluster python=3.8
+conda activate pygmcluster
 cd dev_scripts
 
